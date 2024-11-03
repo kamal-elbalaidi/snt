593f1fa96b614d2050dd026416ab565494761b48
kamal <kamal.elbalaidi@gmail.com>
2024-11-03 01:11:28 +0100

    Del extra line

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define HEIGHT                 "500"                              // the height of the window
 #define FONT                   "JetBrainsMono NFM Medium,\
                                 DejaVuSansM Nerd Font Propo"      // Font(s) family
-#define FONT_SIZE              11                                 // Font size 
+#define FONT_SIZE              11                                 // Font size
 #define FONT_SCALE             1                                  // Leave this like this
 #define SCALE_FACTOR           0.1                                // Leave this like this
 #define CURSOR_AH              TRUE                               // auto hide the cursor while typing
@@ -10,32 +10,31 @@
 #define SCROLL_LINE            1                                  // count lines in window
 #define CURSORSHAPE            2                                  // 2: Block ("█") 4: Underline ("_") 6: Bar ("|")
 // Modifier and Key even
-#define MODIFIER               GDK_CONTROL_MASK|GDK_SHIFT_MASK 
+#define MODIFIER               GDK_CONTROL_MASK|GDK_SHIFT_MASK
 #define COPY                   GDK_KEY_y
 #define PASTE                  GDK_KEY_p
 #define INCREASE_FONT          GDK_KEY_Page_Up
 #define DECREASE_FONT          GDK_KEY_Page_Down
-#define RESET_FONT             GDK_KEY_Home
 #define SCROLL_UP              GDK_KEY_k
 #define SCROLL_DOWN            GDK_KEY_j
+#define RESET_FONT             GDK_KEY_Home
 #define SCROLL_PAGEUP          GDK_KEY_u
 #define SCROLL_PAGEDOWN        GDK_KEY_d
-#define TOGGLEFULLSCREEN       GDK_KEY_f
 // Colors scheme
 #define CLR_0                  0x0c0c0c                           // black 
 #define CLR_1                  0x80202D                           // red
 #define CLR_2                  0x6aa847                           // grean
 #define CLR_3                  0x847e2b                           // yellow
-#define CLR_4                  0x5f5788                           // blue 
+#define CLR_4                  0x5f5788                           // blue
 #define CLR_5                  0x793d75                           // pink
-#define CLR_6                  0x5d9e79                           // cyin  
+#define CLR_6                  0x5d9e79                           // cyin
 #define CLR_7                  0xcccccc                           // white
-#define CLR_8                  0x313131                           // black 
+#define CLR_8                  0x313131                           // black
 #define CLR_9                  0x80202D                           // red
 #define CLR_10                 0x6aa847                           // grean
 #define CLR_11                 0x847e2b                           // yello
-#define CLR_12                 0x5f5788                           // blue 
+#define CLR_12                 0x5f5788                           // blue
 #define CLR_13                 0x793d75                           // pink
-#define CLR_14                 0x5d9e79                           // cyin 
+#define CLR_14                 0x5d9e79                           // cyin
 #define CLR_15                 0xcccccc                           // white
 #define PALETTE_SIZE           16
